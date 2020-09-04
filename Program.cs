43524138597ef2c9885339bd2bb3cb6116e73e31
Jonatas de Oliveira Coelho <jonatas.oliveiracoelho@gmail.com>
2020-09-04 10:51:41 -0300

    Initial Commit

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace hello_world
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "Jonatas";
+            Console.WriteLine($"Hello {name}!");
+            Console.WriteLine("The current time is " + DateTime.Now);
+        }
+    }
+}
